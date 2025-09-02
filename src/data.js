7a9ccd881a6cf17916436a071ad82a6b36175311
Alex Intwoit <intwoit.creative@gmail.com>
2025-09-03 01:10:34 +0300

    update portofoliu

--- src/data.js
+++ src/data.js
@@ -2,19 +2,19 @@ const investments = [
 {
     asset: "ADA",
     entries: [
-      { amount: 300 / 0.82, price: 0.82 },
+      { amount: 600 / 0.82, price: 0.82 },
      ]
   },
 {
     asset: "LINK",
     entries: [
-      { amount: 1350 / 21.48, price: 21.48 },
+      { amount: 1050 / 20.99, price: 20.99 },
      ]
   },
   {
     asset: "FET",
     entries: [
-      { amount: 250 / 0.7592, price: 0.7592 },
+      { amount: 350 / 0.71, price: 0.71 },
     ]
   },
 ];
