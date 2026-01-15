d309de0e4aa63fe7c1d27d8740b38f51af71f1ae
Alex Intwoit <intwoit.creative@gmail.com>
2026-01-15 08:17:05 +0200

    update portofoliu

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ const investments = [
 {
     asset: "BTC",
     entries: [
-      { amount: 1642 / 112858.67, price: 112858.67 },
+      { amount: 1403 / 112858.67, price: 112858.67 },
      ]
   },
 {
@@ -17,12 +17,6 @@ const investments = [
       { amount: 100 / 15.94, price: 15.94 },
      ]
   },
-{
- asset: "SOL",
-    entries: [
-      { amount: 0 / 222.50, price: 222.50 },
-     ]
-  },
 
 
 ];
@@ -31,7 +25,7 @@ export const clientConfig = {
   startingInvestment: 1700,   // baseline fix
   split: { investor: 0.70, manager: 0.30 },
   targetDate: "2025-12-31",
-  cashReserved: 0,
+  cashReserved: 350,
 };
 
 export default investments;
