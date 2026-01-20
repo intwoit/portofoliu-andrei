2e5d8512d6a12450e7c5061dbb3c672987802e12
Alex Intwoit <intwoit.creative@gmail.com>
2026-01-20 23:33:55 +0200

    update portofoliu

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ useEffect(() => {
   });
 
 const totalValue = processedData.reduce((sum, item) => sum + item.value, 0);
-const portfolioInvestment = processedData.reduce((sum, item) => sum + item.investment, 0);
+const coinInvestment = processedData.reduce((sum, item) => sum + item.investment, 0);
 
 // baseline fix de la care începi împărțirea
 const base = STARTING_BASELINE;
